03e7daed5df14e1958ff775329e24c7cc5b7d880
s-tonn <89980486+s-tonn@users.noreply.github.com>
2023-12-21 11:27:52 +0100

    Update IDgenerator.js

--- IDgenerator.js
+++ IDgenerator.js
@@ -1,15 +1,15 @@
 // NO_CB_FACTORS: Number of different counterbalancing factors
-const NO_CB_FAKTORS = 4;
+const NO_CB_FAKTORS = 32;
 
 // EX_CB_FACTORS: permutation modulos already finished (Integer array of values 0 up to (NOT INCLUSIVE) NO_CB_FAKTORS)
 const EX_CB_FAKTORS = [];
-// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 // NOISE: if you want the subject IDs to have a high range, change this parameter
-const NOISE = 199;
+const NOISE = 90;
 
 // OFFSET: if you want the subject IDs to start at an specific value
-const OFFSET = 1200;
+const OFFSET = 3000;
 
 // ~~~~~~~~~ you should never need to change those! ~~~~~~~~~
 function displayIt() {
